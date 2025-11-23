1abb61feeb43e7e7758ec9cc9cff270b38dc2272
Morten Rugaard <morten@rugaard.me>
2025-11-23 16:18:23 +0100

    Move permission request to beginning of script.

--- macos/main.sh
+++ macos/main.sh
@@ -18,6 +18,23 @@ main() {
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    # Check if the terminal has "Full Disk Access", and if it does
+    # not, ask the user to grant it.
+    #
+    # This is required in order to be able to set some preferences.
+    # See also: https://lapcatsoftware.com/articles/containers.html.
+    if ! ls "$HOME/Library/Containers/com.apple.Safari/Data" &> /dev/null; then
+      printf "\n"
+
+      # Open the "Full Disk Access" system preferences.
+      open "x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles"
+
+      # Ask the user to grant "Full Disk Access".
+      ask "Please grant the terminal \"Full Disk Access\" and then press any key to continue. "
+    fi
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   # Homebrew.
 
   ./install/homebrew.sh
@@ -178,21 +195,6 @@ main() {
   fi
 
   if ${SKIP_QUESTIONS} || answer_is_yes; then
-
-    # Check if the terminal has "Full Disk Access", and if it does
-    # not, ask the user to grant it.
-    #
-    # This is required in order to be able to set some preferences.
-    # See also: https://lapcatsoftware.com/articles/containers.html.
-    if ! ls "$HOME/Library/Containers/com.apple.Safari/Data" &> /dev/null; then
-      printf "\n"
-
-      # Open the "Full Disk Access" system preferences.
-      open "x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles"
-
-      # Ask the user to grant "Full Disk Access".
-      ask "Please grant the terminal \"Full Disk Access\" and then press any key to continue. "
-    fi
 
     # Close any open `System Preferences` panes in order to
     # avoid overriding the preferences that are being changed.
